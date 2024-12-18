abb10a18913771b5924b22188a1e1abc75b27be6
Sofian Hidayat <sofiantjkt@gmail.com>
2024-12-19 05:45:21 +0700

    Add files via upload

---
+++ web-portfolio/scripts/main.js
@@ -0,0 +1,20 @@
+//Togge 
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navLists = document.querySelector("nav")
+
+    burger.addEventListener("click", () => { 
+        navLists.classList.toggle("nav-active")
+        burger.classList.toggle("toggle-burger")
+    });
+
+};
+
+navSlide();
+
+// clear for, before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset(); 
+    }
+};
